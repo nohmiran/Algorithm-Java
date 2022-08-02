f8dbbacdf91b3cfded7fc7c5563ab0666811ece3
nohmiran <nmr1227@gmail.com>
2022-08-03 00:23:13 +0900

    8월 3일 롬복API 다운로드까지만 완료

--- SpringMVC03/src/main/java/kr/board/domain/Board.java
+++ SpringMVC03/src/main/java/kr/board/domain/Board.java
@@ -1,52 +1,14 @@
 package kr.board.domain;
 
-public class Board {
+public class Board { // lombok(롬복)
 
 	// property(프로퍼티, 속성)
 	private int idx; // 번호
+	private String memId; // -> 새로 추가 : 사용자ID
 	private String title; // 제목
 	private String contents; // 내용
 	private String writer; // 작성자
 	private String indate; // 작성일
 	private int count; // 조회수
-	
-	// getter, setter
-	public int getIdx() {
-		return idx;
-	}
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContents() {
-		return contents;
-	}
-	public void setContents(String contents) {
-		this.contents = contents;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getIndate() {
-		return indate;
-	}
-	public void setIndate(String indate) {
-		this.indate = indate;
-	}
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
-	}
-	
-	
+		
 }
