1aad8f748aa1161f8c71e01b5a9a70e46659dbc2
nohmiran <nmr1227@gmail.com>
2022-08-31 00:34:19 +0900

    8월31일 백준알고리즘 A+B-7 주석 수정

--- Algorithm/src/Baekjoon/ex28_AplusBminus7.java
+++ Algorithm/src/Baekjoon/ex28_AplusBminus7.java
@@ -10,12 +10,13 @@ public class ex28_AplusBminus7 {
 		
 		int T = sc.nextInt(); // 문제 갯수
 		
-		for(int i = 1; i <= T; i++) {
+		for(int i = 1; i <= T; i++) { // case#뒤에 붙는 수가 1부터 시작하기 때문에 시작 수를 1로 지정.
 			
-			int num1 = sc.nextInt();
-			int num2 = sc.nextInt();
+			int num1 = sc.nextInt(); // 정수 1
+			int num2 = sc.nextInt(); // 정수 2
 			
-			System.out.println("Case #"+i+": "+(num1+num2));
+			// "Case #i: ~"형식으로 출력
+			System.out.println("Case #"+i+": "+(num1+num2)); 
 			
 		}
 		
