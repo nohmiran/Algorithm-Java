ab260089a85e91a5c8e5e791d04d1e62b0781728
nohmiran <nmr1227@gmail.com>
2022-09-27 21:41:28 +0900

    9월 26일 백준 알고리즘 사장님 도박은 재미로 하셔야 합니다(BufferedReader)

---
+++ Algorithm/src/Baekjoon/ex55_사장님도박은재미로하셔야합니다.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex55_사장님도박은재미로하셔야합니다 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+
+		while (true) {
+			int num = Integer.parseInt(br.readLine());
+
+			if (num == -1) {
+				
+				break;
+			
+			}
+			sum += num;
+		}
+		System.out.println(sum);
+	}
+
+}
