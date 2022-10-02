ce0a46f6595cb42b1c1e9f1b8dffe8e3bea306f9
nohmiran <nmr1227@gmail.com>
2022-10-03 00:09:15 +0900

    10월 3일 백준 알고리즘 N찍기

---
+++ Algorithm/src/Baekjoon/ex63_N찍기.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex63_N찍기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt(); // 자연수 N
+		// 1부터 N까지 한 줄에 하나씩 출력
+		
+		for (int i = 1; i <= num; i++) {
+			System.out.println(i);
+			
+		}
+	}
+
+}
