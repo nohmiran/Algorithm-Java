f4339321c8f328a85648276a848935a9ce81ab6d
nohmiran <nmr1227@gmail.com>
2022-10-04 00:09:52 +0900

    10월 4일 백준 알고리즘 기찍N

---
+++ Algorithm/src/Baekjoon/ex64_기찍N.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex64_기찍N {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt(); // 입력한 수
+		
+		// 입력한 수에서 -1씩 1까지 출력
+		for (int i = num; i >= 1; i--) {
+			System.out.println(i);
+
+		}
+	}
+
+}
