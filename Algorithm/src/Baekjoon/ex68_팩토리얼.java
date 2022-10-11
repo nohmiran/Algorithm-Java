03518ae49ebba858c01da4fa21d9102ed077ea98
nohmiran <nmr1227@gmail.com>
2022-10-11 22:48:16 +0900

    10월 11일 백준 알고리즘 팩토리얼

---
+++ Algorithm/src/Baekjoon/ex68_팩토리얼.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex68_팩토리얼 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		// factorial은 모르겠고 그냥 for문으로 작성해봐야지
+		int n = sc.nextInt();
+		
+		int result = 1;
+		
+		for (int i = 1; i <= n; i++) {
+			result *= i;
+			
+		}
+
+		System.out.println(result);
+
+	}
+}
