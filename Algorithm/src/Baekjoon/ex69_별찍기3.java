c02ff777e07acaa13d3d92944b331f7cc41ec48b
nohmiran <nmr1227@gmail.com>
2022-10-12 21:32:16 +0900

    10월 12일 백준 알고리즘 별찍기3

---
+++ Algorithm/src/Baekjoon/ex69_별찍기3.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class ex69_별찍기3 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		for (int i = 0; i < num; i++) {
+
+			for (int j = i + 1; j <= num; j++) {
+
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
