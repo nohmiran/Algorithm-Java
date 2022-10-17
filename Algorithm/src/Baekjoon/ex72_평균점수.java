9e283ef4bfbf5814c95c3435734152d0232057e6
nohmiran <nmr1227@gmail.com>
2022-10-17 23:32:30 +0900

    10월 17일 백준 알고리즘 평균점수

---
+++ Algorithm/src/Baekjoon/ex72_평균점수.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex72_평균점수 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+	        int sum = 0;
+	        int score = 5;
+	        for (int i = 0; i < score; i++) { // 5개 입력
+	            int n = Integer.parseInt(br.readLine());
+	           
+	            if (n < 40) { // 40점보다 작으면 
+	            	n = 40; // 40점으로
+	            }
+	            sum += n; // sum에 모든 점수를 더해주고
+	        }
+	        System.out.print(sum / score); // /5 를 하면 avg가 나온다.
+	    }
+
+	}
