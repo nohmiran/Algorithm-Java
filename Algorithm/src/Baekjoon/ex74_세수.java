5478bd921d43005a4247d07542a7284d3b677898
nohmiran <nmr1227@gmail.com>
2022-10-20 22:51:35 +0900

    10월 20일 백준 알고리즘 세 수

---
+++ Algorithm/src/Baekjoon/ex74_세수.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex74_세수 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int arr[] = {sc.nextInt(), sc.nextInt(), sc.nextInt()}; // 3개의 입력 칸을 만들어준다.
+		
+		Arrays.sort(arr); // 오름차순 정렬
+		
+		System.out.println(arr[1]);
+		
+		
+	}
+
+}
