f196d063adbebed24bef72e28b64633c3988a9c1
nohmiran <nmr1227@gmail.com>
2022-10-22 00:33:37 +0900

    10월 22일 백준 알고리즘 그대로 출력하기

---
+++ Algorithm/src/Baekjoon/ex76_그대로출력하기.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex76_그대로출력하기 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+
+	}
+
+}
