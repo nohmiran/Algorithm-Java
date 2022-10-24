872fab1080419463abcef5e61dd7d4f8a545812d
nohmiran <nmr1227@gmail.com>
2022-10-24 23:34:04 +0900

    10월 24일 백준 알고리즘 별찍기5

---
+++ Algorithm/src/Baekjoon/ex78_별찍기5.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex78_별찍기5 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int star = sc.nextInt();
+
+		for (int i = 1; i <= star; i++) {
+
+			for (int j = i; j < star; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 0; k < i * 2 - 1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
