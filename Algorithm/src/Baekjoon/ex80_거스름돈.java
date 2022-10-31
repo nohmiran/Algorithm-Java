8003c166545a464d1e11b12f1e82bca2298f407a
nohmiran <nmr1227@gmail.com>
2022-10-31 22:02:49 +0900

    10월 31일 백준 알고리즘 거스름돈

---
+++ Algorithm/src/Baekjoon/ex80_거스름돈.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex80_거스름돈 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int money = 1000 - Integer.parseInt(br.readLine()); // 받은 돈
+		
+		int result = 0;
+		
+		int[] arr = {500, 100, 50, 10, 5, 1}; // 거슬러 줄 수 있는 동전
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if(money / arr[i] != 0) { // 나머지가 0이 아니라면
+				
+				result += money / arr[i]; 
+				
+				money %= arr[i]; /// 거슬러줄 수 있는 돈이 있을때까지 나눈다
+			}
+			
+		}
+		System.out.println(result);
+	}
+
+}
