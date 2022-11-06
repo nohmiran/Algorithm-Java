143e7c14704723437b57a411c845b84c22d79ea4
nohmiran <nmr1227@gmail.com>
2022-11-07 00:28:26 +0900

    11월 7일 백준 알고리즘 소인수분해

---
+++ Algorithm/src/Baekjoon/ex85_소인수분해.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex85_소인수분해 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 2; i <= n; i++) {
+
+			while (n % i == 0) {
+
+				System.out.println(i);
+
+				n /= i;
+			}
+		}
+	}
+
+}
