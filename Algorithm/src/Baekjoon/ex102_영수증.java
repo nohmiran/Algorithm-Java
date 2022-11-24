7171378af0b2abb409049dfbf48201a113dbd84e
nohmiran <nmr1227@gmail.com>
2022-11-25 00:06:30 +0900

    11월 25일 백준 알고리즘 영수증

---
+++ Algorithm/src/Baekjoon/ex102_영수증.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex102_영수증 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int result = sc.nextInt();
+		
+		for(int i = 0; i < 9; i++) {
+			result -= sc.nextInt();
+		}
+		System.out.println(result);
+	}
+
+}
