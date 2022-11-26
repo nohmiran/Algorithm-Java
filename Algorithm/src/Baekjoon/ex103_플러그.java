3095b29b3b3c02519b652e221697a8d9b30f664d
nohmiran <nmr1227@gmail.com>
2022-11-27 00:21:42 +0900

    11월 27일 백준 알고리즘 플러그

---
+++ Algorithm/src/Baekjoon/ex103_플러그.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex103_플러그 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int sum = 0;
+		int multitap = sc.nextInt();
+
+		for (int i = 0; i < multitap; i++) {
+			int plug = sc.nextInt(); // 멀티탭 개수
+			sum += plug;
+		}
+		int computer = sum - (multitap - 1);
+		System.out.println(computer);
+	}
+
+}
