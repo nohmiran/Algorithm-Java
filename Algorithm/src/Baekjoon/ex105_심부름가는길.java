9a42339b2f1aac3da424e499e0c2be816169c4c5
nohmiran <nmr1227@gmail.com>
2022-11-28 22:49:07 +0900

    11월 28일 백준 알고리즘 심부름 가는 길

---
+++ Algorithm/src/Baekjoon/ex105_심부름가는길.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex105_심부름가는길 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+
+		for (int i = 0; i < 4; i++) {
+			sum += sc.nextInt();
+		}
+		System.out.println(sum / 60);
+		System.out.println(sum % 60);
+
+	}
+
+}
