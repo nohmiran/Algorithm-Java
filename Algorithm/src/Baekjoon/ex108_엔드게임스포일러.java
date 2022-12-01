3bbb5752a676df3a1452b8a4542905bca695edde
nohmiran <nmr1227@gmail.com>
2022-12-01 22:04:11 +0900

    12월 1일 백준 알고리즘 엔드게임 스포일러

---
+++ Algorithm/src/Baekjoon/ex108_엔드게임스포일러.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex108_엔드게임스포일러 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println("Avengers: Endgame");
+    }
+
+}
