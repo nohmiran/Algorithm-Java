b19d7775f717a1827b013350207e1fa968c59e12
nohmiran <nmr1227@gmail.com>
2022-12-05 21:53:10 +0900

    feat: 12월 5일 백준 알고리즘 16진수

---
+++ Algorithm/src/Baekjoon/ex112_16진수.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex112_16진수 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(Integer.parseInt(str,16)); // 입력된 글자를 16진수로 바꿔줘~
+	}
+
+}
