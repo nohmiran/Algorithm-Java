fe056fb6f0d7361acf7d6274f1f3bb04c5604672
nohmiran <nmr1227@gmail.com>
2022-12-11 00:32:24 +0900

    feat: 12월 11일 백준 알고리즘 立方体(Cube)

---
+++ Algorithm/src/Baekjoon/ex117_立方体.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex117_立方体 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+		System.out.println(num * num * num);
+	}
+
+}
