d8f507863af7abfcf19ad5ac1e12f50bedb173e2
nohmiran <nmr1227@gmail.com>
2022-12-18 23:22:44 +0900

    feat: 12월 18일 백준 알고리즘 오늘 날짜

---
+++ Algorithm/src/Baekjoon/ex124_오늘날짜.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.time.LocalDate;
+
+public class ex124_오늘날짜 {
+	public static void main(String[] args) {
+		
+		LocalDate now = LocalDate.now();
+		
+		System.out.println(now);
+	}
+}
