956747668608fe2c92d78399f6e156cf3bbd338e
nohmiran <nmr1227@gmail.com>
2022-12-29 00:00:41 +0900

    feat: 12월 28일 백준 알고리즘 별찍기7

--- Algorithm/src/Baekjoon/ex134_별찍기7.java
+++ Algorithm/src/Baekjoon/ex134_별찍기7.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class ex134_별찍기7 {
 
 	public static void main(String[] args) {
-		  Scanner scanner = new Scanner(System.in);
-		    int star = scanner.nextInt();
+		  Scanner sc = new Scanner(System.in);
+		    int num = sc.nextInt();
 
-		    for(int i = 1; i <= star; i++) {
-		      for(int j = i; j < star; j++) {
+		    for(int i = 1; i <= num; i++) {
+		      for(int j = i; j < num; j++) {
 		        System.out.print(" ");
 		      }
 		      for(int k = 0; k < i*2-1; k++) {
@@ -18,11 +18,11 @@ public class ex134_별찍기7 {
 		      System.out.println();
 		    }
 
-		    for(int i = 2 ; i <= star; i++) {
+		    for(int i = 2 ; i <= num; i++) {
 		      for(int j = 1; j < i; j++) {
 		        System.out.print(" ");
 		      }
-		      for(int k = (2*star)-(i*2-1); k > 0; k--) {
+		      for(int k = (2*num)-(i*2-1); k > 0; k--) {
 		        System.out.print("*");
 		      }
 		      System.out.println();
