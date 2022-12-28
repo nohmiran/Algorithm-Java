67c6003b2043322c51610ad63884bd0015ff4ae5
nohmiran <nmr1227@gmail.com>
2022-12-29 00:11:38 +0900

    feat: 12월 19일 백준 알고리즘 팰린드롬수

---
+++ Algorithm/src/Baekjoon/ex135_팰린드롬수.java
@@ -0,0 +1,39 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex135_팰린드롬수 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			int num = sc.nextInt();
+		
+			if (num == 0) {
+				break;
+			
+			} else {
+				
+				int count = 0;
+				String str = Integer.toString(num);
+				
+				for (int i = 0; i < str.length() / 2; i++) {
+				
+					if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+					
+						count++;
+					}
+				}
+				if (count == str.length() / 2) {
+					System.out.println("yes");
+
+				} else {
+					System.out.println("no");
+				}
+			}
+		}
+	}
+
+}
