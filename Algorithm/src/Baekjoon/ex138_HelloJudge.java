e1a475764bcb53ad3462aa665ffeffa7aeacb7d7
nohmiran <nmr1227@gmail.com>
2023-01-02 00:01:05 +0900

    feat: 1월 2일 백준 알고리즘 Hello Judge

---
+++ Algorithm/src/Baekjoon/ex138_HelloJudge.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex138_HelloJudge {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			System.out.println("Hello World, Judge " + i + "!");
+		}
+
+	}
+
+}
