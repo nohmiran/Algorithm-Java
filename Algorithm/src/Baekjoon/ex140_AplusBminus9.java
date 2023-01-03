d9bec92239c4a5f63e0d2f13421038f73d00c1e2
nohmiran <nmr1227@gmail.com>
2023-01-04 00:15:10 +0900

    feat: 1월 4일 백준 알고리즘 A+B-9

---
+++ Algorithm/src/Baekjoon/ex140_AplusBminus9.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class ex140_AplusBminus9 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger num1 = new BigInteger(st.nextToken());
+		BigInteger num2 = new BigInteger(st.nextToken());
+		
+		System.out.print(num1.add(num2));
+
+	}
+
+}
