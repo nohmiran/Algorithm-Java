fe9bc458c897aa232cbcd27dc8b9bc897b7d409b
nohmiran <nmr1227@gmail.com>
2023-01-30 00:03:30 +0900

    feat: 1월 30일 백준 알고리즘 유학 금지

---
+++ Algorithm/src/Baekjoon/ex164_유학금지.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex164_유학금지 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.next();
+
+		System.out.println(str.replaceAll("[CAMBRIDGE]", ""));
+
+	}
+
+}
