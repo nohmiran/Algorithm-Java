8ef3eebaa72657864d2a8e59f797fddbf31a5d47
nohmiran <nmr1227@gmail.com>
2023-01-31 21:38:10 +0900

    feat: 1월 31일 백준 알고리즘 너의 이름은 몇 점이니?

---
+++ Algorithm/src/Baekjoon/ex165_너의이름은몇점이니.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex165_너의이름은몇점이니 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int length = sc.nextInt();
+		String name = sc.next();
+
+		int result = 0; // 점수 합산할 변수
+
+		for (int i = 0; i < length; i++) {
+
+			// A의 아스키코드는 65
+			// 문제에서 A는 1
+			// (name.chatAt)-64를 해줘야 한다.
+			result += name.charAt(i) - 64;
+		}
+
+		System.out.println(result);
+	}
+
+}
