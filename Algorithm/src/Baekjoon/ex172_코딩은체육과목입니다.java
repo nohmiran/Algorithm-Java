61fa27ebf85283dffcbc2eebe25336cee46839f2
nohmiran <nmr1227@gmail.com>
2023-02-09 00:09:11 +0900

    feat: 2월 8일 백준 알고리즘 코딩은 체육과목입니다

--- Algorithm/src/Baekjoon/ex172_코딩은체육과목입니다.java
+++ Algorithm/src/Baekjoon/ex172_코딩은체육과목입니다.java
@@ -7,22 +7,25 @@ import java.io.InputStreamReader;
 public class ex172_코딩은체육과목입니다 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		// string으로 입력된 br Integer로 강제 형변환
-//        int n = Integer.parseInt(br.readLine()) / 4;
-//        // 그리고 이를 4로 나누고, 나눈 만큼 long을 출력
-//        // 맨 끝에는 int를 출력.
-//        int str="long ".repeat(n); 
-//        System.out.print( + "int");
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n/4; i++) {
-            sb.append("long ");
-        }
-        sb.append("int");
-        System.out.println(sb);
+		
+		// string으로 입력된 br Integer로 강제 형변환
+		int num = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		// 그리고 이를 4로 나누고, 나눈 만큼 long을 출력
+		// repeat 메소드를 쓰려 했는데 하 씨 안된다 
+		// 자바 11버전부터 안된다고 한다.
+		for (int i = 0; i < num / 4; i++) {
+			sb.append("long ");
+		}
+		
+		// 맨 끝에는 int를 출력하기 위해 append 메소드 쓰기
+		sb.append("int");
+		
+		// 출력
+		System.out.println(sb);
 	}
 
 }
