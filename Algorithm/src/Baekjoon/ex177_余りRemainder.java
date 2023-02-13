f0325d46d5dddb3acc04baef99c62d18cb340ff5
nohmiran <nmr1227@gmail.com>
2023-02-13 23:16:47 +0900

    feat: 2월 13일 백준 알고리즘 余り(Remainder)

---
+++ Algorithm/src/Baekjoon/ex177_余りRemainder.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex177_余りRemainder {
+
+	public static void main(String[] args) {
+		
+		// 외국어로 해놓으믄 뭔 문젠지 내가 어찌고 안다냐
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		// 입력된 수에 21을 나눠 나머지를 출력하는 문제
+		// %를 쓰면 되겄g~
+		System.out.println(num % 21);
+
+	}
+
+}
