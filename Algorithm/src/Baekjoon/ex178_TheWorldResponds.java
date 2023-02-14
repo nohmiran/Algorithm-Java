f0d64147572179b1250170afc5500133655a17b5
nohmiran <nmr1227@gmail.com>
2023-02-14 23:45:27 +0900

    feat: 2월 14일 백준 알고리즘 The World Responds

---
+++ Algorithm/src/Baekjoon/ex178_TheWorldResponds.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+public class ex178_TheWorldResponds {
+
+	public static void main(String[] args) {
+		
+		// hello world는 식상하니까 다른 문장을 출력하라는 문제..... ㅋㅋ..
+		System.out.println("The world says hello!");
+
+	}
+
+}
