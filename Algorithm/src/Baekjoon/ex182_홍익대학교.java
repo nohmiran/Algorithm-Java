38defc6b9fe6ae0753f9becb469ac71c0b86d3f4
nohmiran <nmr1227@gmail.com>
2023-02-17 00:01:55 +0900

    feat: 2월 17일 백준 알고리즘 홍익대학교

---
+++ Algorithm/src/Baekjoon/ex182_홍익대학교.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex182_홍익대학교 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int since = sc.nextInt();
+		
+		System.out.println(since - 1946);
+	
+	}
+
+}
