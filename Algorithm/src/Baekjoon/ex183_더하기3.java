ef0ec2c5ae5c774c8834813742c7f5d54ed66d4c
nohmiran <nmr1227@gmail.com>
2023-02-17 00:09:41 +0900

    feat: 2월 17일 백준 알고리즘 더하기 3

---
+++ Algorithm/src/Baekjoon/ex183_더하기3.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class ex183_더하기3 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int sum = 0;
+
+		while (st.hasMoreTokens()) {
+			sum += Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(sum);
+	}
+
+}
