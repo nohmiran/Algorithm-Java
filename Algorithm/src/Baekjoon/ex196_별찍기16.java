b6190e030608868d460e11a6e9ae207af16a9728
nohmiran <nmr1227@gmail.com>
2023-03-02 21:32:33 +0900

    feat: 3월 2일 백준 알고리즘 별찍기 16

---
+++ Algorithm/src/Baekjoon/ex196_별찍기16.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ex196_별찍기16 {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = i; j <= n - 1; j++) {
+
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i * 2 - 1; j++) {
+
+				if (j % 2 == 1) {
+
+					System.out.print("*");
+				} else {
+
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
