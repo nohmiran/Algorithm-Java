0fb56e736ad6c74a9d6fe7ea201ed84b95bfd286
미란 <97715292+nohmiran@users.noreply.github.com>
2023-03-19 00:07:45 +0900

    feat: 3월 18일 백준 알고리즘 IdentifyingTea

--- Algorithm/src/Baekjoon/ex212_IdentifyingTea.java
+++ Algorithm/src/Baekjoon/ex212_IdentifyingTea.java
@@ -1,20 +1,22 @@
+package Baekjoon;
+
 import java.util.Scanner;
 
-public class ex212_Identifyingtea{
+public class ex212_IdentifyingTea{
     public static void main(String[] args){
         
         Scanner sc = new Scanner(System.in);
        
         int test = sc.nextInt();
         int result = 0;
-
+        
         for(int i = 0; i < 5; i++){
-
+        
             if(sc.nextInt() == test){
-
+        
                 result += 1;
+            }
         }
-      }
         System.out.println(result);
     }
 }
