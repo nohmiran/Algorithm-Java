0b34d0560e2c0e17865c811aeb772eeca9ab8d35
미란 <97715292+nohmiran@users.noreply.github.com>
2023-03-20 23:32:17 +0900

    feat: 3월 20일 백준 알고리즘 문자와 문자열

---
+++ Algorithm/src/Baekjoon/ex214_문자와문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class ex214_문자와문자열 {
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine().trim();
+		int num = Integer.parseInt(br.readLine());
+		
+		System.out.println(str.substring(num - 1, num));
+	}
+	
+	
+}
