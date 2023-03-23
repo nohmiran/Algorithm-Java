ad1c652e67034ac3025176b52e11f4c29f6b74d6
미란 <97715292+nohmiran@users.noreply.github.com>
2023-03-23 23:57:58 +0900

    feat: 3월 23일 백준 알고리즘 벌집

---
+++ Algorithm/src/Baekjoon/ex217_벌집.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class ex217_벌집 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int meter = 1;
+        int range = 2;
+
+        if (num == 1) {
+            System.out.println(1);
+        } else {
+            while (range <= num) {
+                range = range + (6 * meter);
+                meter++;
+            }
+            System.out.println(meter);
+        }
+    }
+}
