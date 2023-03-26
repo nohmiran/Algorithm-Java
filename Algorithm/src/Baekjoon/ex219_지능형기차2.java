aca3f8cf9a74044c5be3472c9c6c38c6793fc73c
미란 <97715292+nohmiran@users.noreply.github.com>
2023-03-26 23:45:25 +0900

    Create ex219_지능형기차2.java
    feat: 3월 26일 백준 알고리즘 지능형 기차 2

---
+++ Algorithm/src/Baekjoon/ex219_지능형기차2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ex219_지능형기차2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        int max = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+            
+            num -= out;
+            num += in;
+            
+            if (num > max){
+                max = num;
+            } 
+        }
+        System.out.println(max);
+    }
+}
