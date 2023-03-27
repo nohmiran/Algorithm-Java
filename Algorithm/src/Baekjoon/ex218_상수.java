65e45a539828d177be01ea8aeee4009c7db21c38
nohmiran <nmr1227@gmail.com>
2023-03-27 23:42:22 +0900

    feat: 3월 24일 백준 알고리즘 상수

--- Algorithm/src/Baekjoon/ex218_상수.java
+++ Algorithm/src/Baekjoon/ex218_상수.java
@@ -15,7 +15,7 @@ public class ex218_상수{
 		num1 = Integer.parseInt(new StringBuilder().append(num1).reverse().toString());
 		num2 = Integer.parseInt(new StringBuilder().append(num2).reverse().toString());
 		
-        // num1이 num2보다 크다면 num1 출력, 그렇지 않다면 num2 출력
+        // num1이 num2보다 크다면 num1 출력. 그렇지 않다면 num2 출력
 		System.out.print(num1 > num2 ? num1 : num2);
 	
 	}
