0ac00f07503ab85041d40196c0493f400a543bec
nohmiran <nmr1227@gmail.com>
2023-03-27 23:42:57 +0900

    feat: 3월 26일 백준 알고리즘 지능형 기차2

--- Algorithm/src/Baekjoon/ex219_지능형기차2.java
+++ Algorithm/src/Baekjoon/ex219_지능형기차2.java
@@ -1,9 +1,14 @@
+package Baekjoon;
+
 import java.util.Scanner;
 
 public class ex219_지능형기차2 {
+	
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = 0;
+    
+    	Scanner sc = new Scanner(System.in);
+        
+    	int num = 0;
         int max = 0;
         
         for (int i = 0; i < 10; i++) {
@@ -18,6 +23,7 @@ public class ex219_지능형기차2 {
                 max = num;
             } 
         }
+
         System.out.println(max);
     }
 }
