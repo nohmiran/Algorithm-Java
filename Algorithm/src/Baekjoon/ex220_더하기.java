b561a51b94d60a501f03e7669c2bdf867556be50
nohmiran <nmr1227@gmail.com>
2023-03-27 23:43:19 +0900

    feat: 3월 27일 백준 알고리즘 더하기

---
+++ Algorithm/src/Baekjoon/ex220_더하기.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex220_더하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a + b);
+	}
+
+}
