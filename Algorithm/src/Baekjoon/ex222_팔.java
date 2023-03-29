57535d728a22678cac6bcdaf7a0246b93e86c8b1
미란 <97715292+nohmiran@users.noreply.github.com>
2023-03-29 23:48:12 +0900

    feat: 3월 29일 백준 알고리즘 팔

---
+++ Algorithm/src/Baekjoon/ex222_팔.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ex222_팔 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String str1 = st.nextToken();
+		String str2 = st.nextToken();
+		
+		int count = 0;
+		
+		if (str1.length() == str2.length()) {
+			for (int i = 0; i < str1.length(); i++) {
+				if (str1.charAt(i) != str2.charAt(i)) {
+			        break;
+			    }	
+				if (str1.charAt(i) == '8') {
+			        count++;
+			    }
+			}
+		}
+		System.out.println(count);
+	
+	}
+
+}
