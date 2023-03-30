d83b7d8b45ee57656e8049de3e94e39a0ec19c3f
미란 <97715292+nohmiran@users.noreply.github.com>
2023-03-30 13:57:02 +0900

    feat: 3월 30일 백준 알고리즘 1로 만들기

---
+++ Algorithm/src/Baekjoon/ex223_1로만들기.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ex223_1로만들기 {
+    
+    public static void main(String[] args) throws IOException {
+        
+     Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+
+        arr[1] = 0;
+
+        for (int i = 2; i < arr.length; i++) {
+
+            arr[i] = arr[i - 1] + 1;
+
+            if (i % 3 == 0) {
+                
+                if (arr[i / 3] + 1 < arr[i]) {
+                    
+                    arr[i] = arr[i / 3] + 1;
+                }
+            }
+            if (i % 2 == 0) {
+                
+                if (arr[i / 2] + 1 < arr[i]) {
+                    
+                    arr[i] = arr[i / 2] + 1;
+                }
+            }
+        }
+        System.out.println(arr[n]);
+    }
+}
