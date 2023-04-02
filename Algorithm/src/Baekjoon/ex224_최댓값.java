1cfe77cd09e4d3041792c07d0383779cbd4ce751
nohmiran <nmr1227@gmail.com>
2023-04-02 18:02:48 +0900

    feat: 3월 31일 백준 알고리즘 최댓값

--- Algorithm/src/Baekjoon/ex224_최댓값.java
+++ Algorithm/src/Baekjoon/ex224_최댓값.java
@@ -1,28 +1,29 @@
+package Baekjoon;
+
 import java.util.Scanner;
 
 public class ex224_최댓값 {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int num[][] = new int[10][10];
-        
+
 		int max = 0; // 최대값
-        
+
 		int x = 0;
-    int y = 0; // 좌표
+		int y = 0; // 좌표
 
 		for (int i = 0; i < 9; i++) {
-            
+
 			for (int j = 0; j < 9; j++) {
-                
+
 				num[i][j] = sc.nextInt();
-				
-                if (max < num[i][j]) {
+
+				if (max < num[i][j]) {
 					max = num[i][j];
-					x = i; 
-          y = j;
+					x = i;
+					y = j;
 				}
 			}
 		}
