bac89dc082a7d7d48e36e62dce4df8c385f69bef
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-05 22:59:40 +0900

    feat: 4월 5일 백준 알고리즘 수학은 비대면강의입니다

---
+++ Algorithm/src/Baekjoon/ex229_수학은비대면강의입니다.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex229_수학은비대면강의입니다 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+
+        System.out.println(x + " " + y);
+    }
+}
