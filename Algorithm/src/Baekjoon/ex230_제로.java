dd5922fa83d4736b5aa551df3dcc721164f86901
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-07 08:27:34 +0900

    feat: 4월 6일 백준 알고리즘 제로

---
+++ Algorithm/src/Baekjoon/ex230_제로.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class Main {
+    
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<>();
+		int test = sc.nextInt();
+		int result = 0;
+		
+        for (int i = 0; i < test; i++) {
+			int num = sc.nextInt();
+			if(num == 0) { // 정수가 "0" 일 경우에는 가장 최근에 쓴 수를 지우고
+				stack.pop();
+			}else { // 아닐 경우 해당 수를 쓴다.
+				stack.push(num);
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			result += stack.peek(); // 스택에 남아있는 수 합산.
+			stack.pop();
+		}
+
+		System.out.println(result);
+	}
+}
