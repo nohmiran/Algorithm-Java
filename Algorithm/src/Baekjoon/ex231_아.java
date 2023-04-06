7cb9d2963c3fe72ffce5d476d5d3c1e09e876859
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-07 08:40:18 +0900

    feat: 4월 7일 백준 알고리즘 아!

---
+++ Algorithm/src/Baekjoon/ex231_아.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex231_아 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String a = sc.next();
+		String b = sc.next();
+		
+		if (a.length() >= b.length()) {
+			System.out.println("go");
+		}else {
+			System.out.println("no");
+		}
+	}
+}
