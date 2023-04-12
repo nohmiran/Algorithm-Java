1f32d0230b5e23f07ab41626731b5fb59ac5a211
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-12 09:36:54 +0900

    feat: 4월 12일 백준 알고리즘 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ Algorithm/src/Baekjoon/ex236_알고리즘수업_알고리즘의수행시간2.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex236_알고리즘수업_알고리즘의수행시간2 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+    
+        System.out.println(num);
+        System.out.println(1);
+    }
+}
