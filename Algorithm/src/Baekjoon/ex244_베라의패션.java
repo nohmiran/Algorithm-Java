aaf1d484c2532596ac3f21d800ad44df1210797b
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-20 23:58:03 +0900

    feat: 4월 20일 백준 알고리즘 베라의 패션

---
+++ Algorithm/src/Baekjoon/ex244_베라의패션.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ex244_베라의패션 {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(num * (num - 1));
+    }    
+}
