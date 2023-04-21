e1bcb38adacbfafb612b2cfdcf58275d9c1e9296
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-21 23:15:32 +0900

    feat: 4월 21일 백준 알고리즘 팩토리얼 2

---
+++ Algorithm/src/Baekjoon/ex245_팩토리얼2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ex245_팩토리얼2 {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextInt();
+        long fac = 1;
+
+        for(int i = 1; i <= num; i++){
+                fac *= i;
+        }
+
+        System.out.println(fac);
+    }    
+}
