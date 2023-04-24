68d2d20f583e1b3550bd9c90eb83dbb7d9dadf2d
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-24 23:34:44 +0900

    feat: 4월 24일 백준 알고리즘 수학은 체육과목 입니다

---
+++ Algorithm/src/Baekjoon/ex246_수학은체육과목입니다.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex246_수학은체육과목입니다 {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		System.out.print(Long.parseLong(br.readLine()) * 4);
+	}
+}
