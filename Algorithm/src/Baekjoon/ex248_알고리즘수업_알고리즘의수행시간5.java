8ce7873629d4b8a54bf645a033c0a0f68a1ec069
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-26 23:32:59 +0900

    feat: 4월 26일 백준 알고리즘 알고리즘 수업 - 알고리즘의 수행 시간 5

---
+++ Algorithm/src/Baekjoon/ex248_알고리즘수업_알고리즘의수행시간5.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class ex248_알고리즘수업_알고리즘의수행시간5 {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        long num = Long.parseLong(br.readLine());
+
+        System.out.println(num * num * num);
+        System.out.println(3);
+    }
+}
