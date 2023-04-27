bc7eb2c19c990c76704572462044b286d52ac6f8
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-27 23:06:12 +0900

    feat: 4월 27일 백준 알고리즘 약수들의 합

---
+++ Algorithm/src/Baekjoon/ex249_약수들의합.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ex249_약수들의합 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while(true) {
+            StringBuilder sb = new StringBuilder();
+            int n = sc.nextInt();
+            if(n == -1) {
+                 break;
+            }
+            
+            int sum = 0;
+
+            for(int i=1; i<n; i++)
+                if(n % i == 0) {
+                    sb.append(i + " + ");
+                    sum += i;
+                }
+            System.out.println(n == sum ? n + " = " + sb.toString().substring(0, sb.length()-3) : n + " is NOT perfect.");
+        }
+    }
