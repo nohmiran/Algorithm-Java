b2f06f7caa36bc6938b6bb90120c7cc5afb79f2f
미란 <97715292+nohmiran@users.noreply.github.com>
2023-04-29 21:24:40 +0900

    feat: 4월 29일 백준 알고리즘 그릇

---
+++ Algorithm/src/Baekjoon/ex251_그릇.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class ex251_그릇 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        
+        int height = 10;
+        
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i-1)) {
+                height += 5;
+            } else {
+                height += 10;
+            }
+        }
+        System.out.print(height);
+    }
+}
