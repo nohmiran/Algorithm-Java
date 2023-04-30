f0a9063a19e3609133025d33688959a1cc660e91
nohmiran <nmr1227@gmail.com>
2023-04-30 23:24:56 +0900

    feat: 4월 17일 백준 알고리즘 알고리즘 수업 - 알고리즘의 수행 시간 1

--- Algorithm/src/Baekjoon/ex241_알고리즘수업_알고리즘의수행시간1.java
+++ Algorithm/src/Baekjoon/ex241_알고리즘수업_알고리즘의수행시간1.java
@@ -1,10 +1,10 @@
 package Baekjoon;
 
 public class ex241_알고리즘수업_알고리즘의수행시간1 {
-    
-    public static void main(String[] args) throws IOException {
-        	
-                System.out.println('1');
+
+	public static void main(String[] args) {
+
+		System.out.println('1');
 		System.out.println('0');
 	}
 }
