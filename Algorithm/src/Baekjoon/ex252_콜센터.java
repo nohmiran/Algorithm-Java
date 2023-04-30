316c2dc5ad783ef963fc65b59cfd2ba44548e1e4
nohmiran <nmr1227@gmail.com>
2023-04-30 23:19:12 +0900

    feat: 4월 30일 백준 알고리즘 콜센터

---
+++ Algorithm/src/Baekjoon/ex252_콜센터.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+public class ex252_콜센터 {
+	
+    public static void main(String[] args) {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
