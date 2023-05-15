907e13901e0215b6001cfc7bb066e950800d512c
미란 <97715292+nohmiran@users.noreply.github.com>
2023-05-15 09:26:00 +0900

    feat: 5월 15일 백준 알고리즘 다이얼

---
+++ Algorithm/src/Baekjoon/ex260_다이얼.java
@@ -0,0 +1,55 @@
+package Baekjoon;
+
+import java.util.Scanner;
+ 
+public class ex260_다이얼 {
+    
+	public static void main(String[] args) {
+    
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+ 
+		int count = 0;
+		int num = str.length();
+        
+		for(int i = 0; i < num; i++) {
+        
+			switch(str.charAt(i)) {
+            
+			case 'A' : case 'B': case 'C' : 
+				count += 3;
+				break;
+                
+			case 'D' : case 'E': case 'F' :
+				count += 4; 
+				break;
+                
+			case 'G' : case 'H': case 'I' :
+				count += 5; 
+				break;
+                
+			case 'J' : case 'K': case 'L' : 
+				count += 6;
+				break;
+                
+			case 'M' : case 'N': case 'O' :
+				count += 7;
+				break;
+                
+			case 'P' : case 'Q': case 'R' : case 'S' : 
+				count += 8; 
+				break;
+                
+			case 'T' : case 'U': case 'V' : 
+				count += 9;
+				break;
+                
+			case 'W' : case 'X': case 'Y' : case 'Z' : 
+				count += 10;
+				break;
+			}
+		}		
+		System.out.print(count);
+	}
+}
