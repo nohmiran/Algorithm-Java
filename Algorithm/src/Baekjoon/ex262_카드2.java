c20f4ab18a2af2058b6ab383bcc33f5e1e918565
미란 <97715292+nohmiran@users.noreply.github.com>
2023-05-16 09:27:03 +0900

    feat: 5월 16일 백준 알고리즘 카드2

---
+++ Algorithm/src/Baekjoon/ex262_카드2.java
@@ -0,0 +1,25 @@
+package Beakjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class ex262_카드2 {
+    
+	public static void main(String[] args) throws IOException{
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		int num = Integer.parseInt(br.readLine());	
+		
+		for(int i = 1; i <= num; i++)
+			queue.offer(i);
+		
+		while(queue.size() > 1) {
+			queue.poll();
+			queue.offer(queue.poll());
+		}
+		System.out.println(queue.poll());
+	}
+}
