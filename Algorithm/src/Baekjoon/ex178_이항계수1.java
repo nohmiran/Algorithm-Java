6e306a3e030509cb5474a49606463e0d846b27b6
미란 <97715292+nohmiran@users.noreply.github.com>
2023-05-30 17:35:55 +0900

    feat: 5월 30일 백준 알고리즘 이항계수 1

---
+++ Algorithm/src/Baekjoon/ex178_이항계수1.java
@@ -0,0 +1,24 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex178_이항계수1 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int answer = 1;
+        for (int i = 1; i <= num1; i++) {
+            answer *= i;
+        }
+        for (int i = 1; i <= num2; i++) {
+            answer /= i;
+        }
+        for (int i = 1; i <= num1 - num2; i++) {
+            answer /= i;
+        }
+        System.out.println(answer);
+    }
+}
