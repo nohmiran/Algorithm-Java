57b4694aebf316e4877af206bf008f17a7c853ca
미란 <97715292+nohmiran@users.noreply.github.com>
2023-06-07 09:45:55 +0900

    feat: 6월 7일 백준 알고리즘 피보나치 수

---
+++ Algorithm/src/Baekjoon/ex283_피보나치수.java
@@ -0,0 +1,23 @@
+import java.io. *;
+import java.math.*;
+import java.util. *;
+
+public class ex283_피보나치수 {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        BigInteger[] dp = new BigInteger[num + 1];
+        dp[0] = BigInteger.ZERO;
+        dp[1] = BigInteger.ONE;
+        
+        if (num >= 2) {
+            dp[2] = BigInteger.ONE;
+        }
+        for (int i = 3; i <= num; i++) {
+            dp[i] = dp[i - 1].add(dp[i - 2]); // 피보나치 수
+        }
+        System.out.println(dp[num]);
+    }
+}
