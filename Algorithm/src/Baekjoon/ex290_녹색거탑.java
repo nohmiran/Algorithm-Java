b9bb19ff24c71787e31c1f4295d3a17463b9f366
미란 <97715292+nohmiran@users.noreply.github.com>
2023-06-09 09:41:34 +0900

    feat: 6월 9일 백준 알고리즘 녹색거탑

---
+++ Algorithm/src/Baekjoon/ex290_녹색거탑.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex290_녹색거탑 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        System.out.println((int) Math.pow(2,input)); // Math.pow : 제곱함수
+    }
+}
