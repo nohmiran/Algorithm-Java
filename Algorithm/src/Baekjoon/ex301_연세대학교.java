d2682ec142a1459a17f6b981c5b38344032ea116
미란 <97715292+nohmiran@users.noreply.github.com>
2023-06-21 13:51:23 +0900

    feat: 6월 21일 백준 알고리즘 연세대학교

---
+++ Algorithm/src/Baekjoon/ex301_연세대학교.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex301_연세대학교 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num == 0) {
+			System.out.println("YONSEI");
+		}
+		if(num == 1) {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+}
