5624f43882eb17bb207e0159f3cabaeb62b7ab51
미란 <97715292+nohmiran@users.noreply.github.com>
2023-07-18 15:20:49 +0900

    feat: 7월 18일 백준 알고리즘 화성 수학

--- Algorithm/src/Baekjoon/ex326_화성수학.java
+++ Algorithm/src/Baekjoon/ex326_화성수학.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ex325_화성수학 {
+public class ex326_화성수학 {
 
     public static void main(String[] args) throws IOException {
         
