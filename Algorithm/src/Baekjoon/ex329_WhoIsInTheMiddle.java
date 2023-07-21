2ff54f8b3d7bf55d97bd480a232aad5c3d8c9b39
미란 <97715292+nohmiran@users.noreply.github.com>
2023-07-21 20:18:39 +0900

    feat: 7월 21일 백준 알고리즘 Who is in the middle?

---
+++ Algorithm/src/Baekjoon/ex329_WhoIsInTheMiddle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ex329_WhoIsInTheMiddle {
+    
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+		arr[0] = sc.nextInt();
+		arr[1] = sc.nextInt();
+		arr[2] = sc.nextInt();
+        
+		Arrays.sort(arr);
+		System.out.print(arr[1]);
+	}
+}
