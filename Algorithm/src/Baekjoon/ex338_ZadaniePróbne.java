45bd7da0fdc4dd0d462f80a506cafd75ae965729
미란 <97715292+nohmiran@users.noreply.github.com>
2023-07-30 16:18:37 +0900

    feat: 7월 30일 백준 알고리즘 Zadanie próbne

---
+++ Algorithm/src/Baekjoon/ex338_ZadaniePróbne.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex338_ZadaniePróbne {
+	
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuffer sb = new StringBuffer(br.readLine());
+
+        System.out.println(sb.reverse());	
+	}	
+}
