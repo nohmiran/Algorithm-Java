8f873a8afed6ead6c0e89c992d407cd482f1c8cb
미란 <97715292+nohmiran@users.noreply.github.com>
2023-08-01 09:30:53 +0900

    feat: 8월 1일 백준 알고리즘 SciComLove

---
+++ Algorithm/src/Baekjoon/ex341_SciComLove.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex341_SciComLove {
+    
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        
+		System.out.println("SciComLove\n".repeat(n)); // repeat : 문자열을 n번 반복해주는 함수
+	}
+}
