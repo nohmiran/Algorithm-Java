b9dac29525c09e080dc4003b375ab42ef403054d
미란 <97715292+nohmiran@users.noreply.github.com>
2023-08-12 23:15:59 +0900

    feat: 8월 12일 백준 알고리즘 CASIO

---
+++ Algorithm/bin/Baekjoon/ex254_CASIO.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex254_CASIO {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        System.out.println(n == m ? 1 : 0);
+    }
+}
