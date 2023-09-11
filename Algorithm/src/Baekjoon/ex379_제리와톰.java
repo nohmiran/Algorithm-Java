addc87accfa69d71735b77c56bd262f5345cb57d
미란 <97715292+nohmiran@users.noreply.github.com>
2023-09-11 09:44:33 +0900

    feat: 9월 11일 백준 알고리즘 제리와 톰

---
+++ Algorithm/src/Baekjoon/ex379_제리와톰.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class ex379_제리와톰 {
+    
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+        
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(b - a + " " + b);
+	}
+}
