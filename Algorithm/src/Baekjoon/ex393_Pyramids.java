694794cfdbac0e33eef7ab1205244b7734293be4
미란 <97715292+nohmiran@users.noreply.github.com>
2023-09-24 18:53:48 +0900

    feat: 9월 24일 백준 알고리즘 Pyramids

---
+++ Algorithm/src/Baekjoon/ex393_Pyramids.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex393_Pyramids {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext())b{
+            int nb=bsc.nextInt();
+
+            if(n == 0){
+                break;
+            }
+
+            int answer=n * (n + 1) / 2;
+            System.out.println(answer);
+        }
+    }
+}
