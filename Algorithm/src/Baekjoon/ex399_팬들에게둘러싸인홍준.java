672bc64214305b1d0cf39794512e69c83c623dca
미란 <97715292+nohmiran@users.noreply.github.com>
2023-09-30 00:06:31 +0900

    feat: 9월 29일 백준 알고리즘 팬들에게 둘러싸인 홍준

---
+++ Algorithm/src/Baekjoon/ex399_팬들에게둘러싸인홍준.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex399_팬들에게둘러싸인홍준 {
+
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::" + str + "::fan:");
+		System.out.println(":fan::fan::fan:");
+	}
+}
