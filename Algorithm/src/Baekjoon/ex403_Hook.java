d8db0bdadc3cf09327c7e1837f9f42e47e7705b8
미란 <97715292+nohmiran@users.noreply.github.com>
2023-10-02 00:09:39 +0900

    feat: 10월 2일 백준 알고리즘 Hook

---
+++ Algorithm/src/Baekjoon/ex403_Hook.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+public class ex403_Hook {
+    
+	public static void main(String[] args) {
+        
+		System.out.println("#  # #### #### #  #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#  # #### #### #  #");
+	}
+}
