fcdb9eee80ae14d3c5f93572e6af7ae94fea0728
미란 <97715292+nohmiran@users.noreply.github.com>
2023-11-07 10:12:04 +0900

    feat: 11월 7일 백준 알고리즘 Serca

---
+++ Algorithm/src/Baekjoon/ex431_Serca.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class ex431_Serca {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        while(num-- > 0) {
+            System.out.println(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     ");
+        }
+    }
+}
