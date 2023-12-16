d8dcd0cf60587d6922604e74f8ed944ea7302e6c
미란 <97715292+nohmiran@users.noreply.github.com>
2023-12-16 21:37:06 +0900

    feat: 12월 16일 백준 알고리즘 Time to Decompress

---
+++ Algorithm/src/Baekjoon/ex469_TimetoDecompress.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ex469_TimetoDecompress {
+
+  public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int test = Integer.parseInt(br.readLine());
+
+        while (test-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < num; i++) {
+                result.append(str);
+            }
+            bw.write(result.toString() + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
