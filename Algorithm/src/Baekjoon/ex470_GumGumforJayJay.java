c1c64c984821e4a1666b4622dd0aee2958711da9
미란 <97715292+nohmiran@users.noreply.github.com>
2023-12-17 23:53:35 +0900

    feat: 12월 17일 백준 알고리즘 Gum Gum for Jay Jay

---
+++ Algorithm/src/Baekjoon/ex470_GumGumforJayJay.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ex470_GumGumforJayJay {
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        
+        while (true) {
+            String temp = br.readLine();
+            if (temp == null) {
+               break;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
+}
