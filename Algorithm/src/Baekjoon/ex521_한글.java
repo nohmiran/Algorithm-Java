6e9ad16fa6b3c8932812b4d98f4bf393a673d726
미란 <97715292+nohmiran@users.noreply.github.com>
2024-02-08 09:46:06 +0900

    feat: 2월 8일 백준 알고리즘 한글

---
+++ Algorithm/src/Baekjoon/ex521_한글.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class ex521_한글 {
+    
+	public static void main(String[] args) {
+        
+		Scanner sc = new Scanner(System.in);
+		System.out.println((char)(sc.nextInt() - 1 + '가'));
+	}
+}
