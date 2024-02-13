6de1beeca752e3dff9b1daf8b327c2b31f357378
미란 <97715292+nohmiran@users.noreply.github.com>
2024-02-13 09:38:42 +0900

    feat: 2월 13일 백준 알고리즘 時間 (hour)

---
+++ Algorithm/src/Baekjoon/ex522_時間.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex522_時間 {
+    
+	public static void main(String[] args) throws Exception {
+        
+		Scanner sc = new Scanner(System.in);
+		int day = sc.nextInt();
+        int hour = day * 24;
+        
+		System.out.println(hour);
+	}
+}
