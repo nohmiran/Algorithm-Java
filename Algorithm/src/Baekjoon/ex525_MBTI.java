9ff4121a105754633b1043b3d0a5e4a5e101441e
미란 <97715292+nohmiran@users.noreply.github.com>
2024-02-15 09:32:03 +0900

    feat: 2월 15일 백준 알고리즘 MBTI

---
+++ Algorithm/src/Baekjoon/ex525_MBTI.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        int num = Integer.parseInt(sc.nextLine());
+        int count = 0;
+        
+        for(int i = 0; i < num; ++i) {
+            if(sc.nextLine().equals(str)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
