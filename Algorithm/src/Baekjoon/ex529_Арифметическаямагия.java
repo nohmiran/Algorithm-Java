b38c0497dd3ffb52009ed50d3077ce4a734ed9b0
미란 <97715292+nohmiran@users.noreply.github.com>
2024-02-21 09:46:55 +0900

    feat: 2월 21일 백준 알고리즘 Арифметическая магия

---
+++ Algorithm/src/Baekjoon/ex529_Арифметическаямагия.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class ex529_Арифметическаямагия {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        sc.next();
+        System.out.println(1);
+    }
+}
