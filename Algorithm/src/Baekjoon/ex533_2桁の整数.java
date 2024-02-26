be0a20c73b92b6bbf7a520462fa22a90cebe6794
미란 <97715292+nohmiran@users.noreply.github.com>
2024-02-26 16:01:06 +0900

    feat: 2월 26일 백준 알고리즘 2 桁の整数 (Two-digit Integer)

---
+++ Algorithm/src/Baekjoon/ex533_2桁の整数.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex533_2桁の整数 {
+    
+	public static void main(String[] args) throws Exception {
+        
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a * 10 + b);
+	}
+}
