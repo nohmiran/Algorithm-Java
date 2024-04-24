0c7510e417001047b9cc6aff4d7cba630e71d8d8
미란 <97715292+nohmiran@users.noreply.github.com>
2024-04-24 17:21:01 +0900

    feat: 4월 24일 백준 알고리즘 Zadanie próbne 2

---
+++ Algorithm/src/Baekjoon/ex570_ZadaniePróbne2.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class ex570_ZadaniePróbne2 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%d %d", (n + 1) * 2, (n + 1) * 3);
+    }
+}
