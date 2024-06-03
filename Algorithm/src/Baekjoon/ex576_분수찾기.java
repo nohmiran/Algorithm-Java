9622ef70f3b40f1a6e12a36cca321781dcca8319
미란 <97715292+nohmiran@users.noreply.github.com>
2024-06-03 09:51:26 +0900

    feat: 6월 3일 백준 알고리즘 분수찾기

---
+++ Algorithm/src/Baekjoon/ex576_분수찾기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ex576_분수찾기 {
+    
+    public static void main(String [] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int l = 1;
+        int s = 0;
+
+        while (n > s + l) {
+            s += l++;
+        }
+      
+        if (l % 2 == 1) { // 홀수일 경우 분자와 분모 출력
+            System.out.println((l - (n - s - 1)) + "/" + (n - s));
+        } else { // 짝수일 경우 분자와 분모 출력
+            System.out.println((n - s) + "/" + (l - (n - s - 1)));
+        }
+    }
+}
