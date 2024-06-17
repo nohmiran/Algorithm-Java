bb2853efc04c793595d6c63c8a59d2e989a729fd
미란 <97715292+nohmiran@users.noreply.github.com>
2024-06-17 15:32:27 +0900

    feat: 6월 17일 백준 알고리즘 8진수, 10진수, 16진수

---
+++ Algorithm/src/Baekjoon/ex578_8진수10진수16진수.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class ex578_8진수10진수16진수 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String str2 = "";
+
+        if (str.charAt(0) == '0' && str.charAt(1) == 'x') {
+            str2 = str.substring(2, str.length());
+            System.out.println(Integer.valueOf(str2, 16));
+        } else if (str.charAt(0) == '0') {
+            str2 = str.substring(1, str.length());
+            System.out.println(Integer.valueOf(str2, 8));
+
+        } else {
+            str2 = str;
+            System.out.println(Integer.valueOf(str2, 10));
+        }
+    }
+}
